d2612e57e0d9af7c4b164836a10a60c99c5c6a64
Yuri Bernardo <yuribernardosie@gmail.com>
2025-04-22 21:39:26 -0300

    Modificações realizadas em contador.digitaljs

--- contador.v
+++ contador.v
@@ -8,6 +8,18 @@ module Contador (
     output reg [7:0] saida
 );
 
-// Insira seu código aqui
+
+   always @(posedge clk or negedge rst_n) begin 
+    if (!rst_n) begin
+        saida <= 8'h6a; 
+    end else begin
+        if(acrescer && !decrecer) begin 
+            saida <= saida + 1;
+        end
+        if(!acrescer && decrecer) begin  
+            saida <= saida - 1; 
+        end 
+    end
+   end
 
 endmodule
